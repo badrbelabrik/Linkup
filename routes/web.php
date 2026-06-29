378556feb4b86cb66c29b408ba67302f6407c0f7
badrbelabrik <badr.belabrik@gmail.com>
2026-06-29 11:57:35 +0100

    create authcontroller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('register', [AuthController::class, 'register']);
+Route::get('login', [AuthController::class, 'login']);
 
 Route::get('/', [PostController::class, 'index']);
 Route::post('/createPost', [PostController::class,'createPost']);
